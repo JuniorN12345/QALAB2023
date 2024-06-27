77c413d20fb79d6134b86afdabde5cbc4284854d
JuniorN12345 <josalc.25@gmail.com>
2024-06-27 01:13:44 -0500

    Archivo

---
+++ SUMA/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 9;
+        int numero2 = 75;
+
+        int suma = numero1 + numero2;
+
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
